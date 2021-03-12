3bddbc343a24a8197623ea8a61419e7ad557d0ee
Mathias Lenz <mathias.lenz@posteo.de>
2021-03-12 13:56:48 +0100

    Delete shopping list items by clicking them

--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -1,12 +1,11 @@
+import kotlinext.js.jsObject
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import react.RProps
+import kotlinx.html.js.onClickFunction
+import react.*
 import react.dom.h1
 import react.dom.li
 import react.dom.ul
-import react.functionalComponent
-import react.useEffect
-import react.useState
 
 private val scope = MainScope()
 
@@ -27,7 +26,25 @@ val App = functionalComponent<RProps> { _ ->
             li {
                 key = item.toString()
                 +"[${item.priority}] ${item.description} "
+                attrs.onClickFunction = {
+                    scope.launch {
+                        deleteShoppingListItem(item)
+                        setShoppingList(getShoppingList())
+                    }
+                }
             }
         }
     }
+    child(
+        InputComponent,
+        props = jsObject {
+            onSubmit = { input ->
+                val cartItem = ShoppingListItem(input.replace("!", ""), input.count { it == '!' })
+                scope.launch {
+                    addShoppingListItem(cartItem)
+                    setShoppingList(getShoppingList())
+                }
+            }
+        }
+    )
 }
